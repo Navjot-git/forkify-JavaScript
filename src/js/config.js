ae70232b00288a1dbaeb9867a4ca9aeb30cd2ca2
navjotsingh <dha3790@mylaurier.ca>
2023-04-01 09:44:28 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '729fee21-97c7-4fab-a637-51ca6ce58008';
+export const MODAL_CLOSE_SEC = 2.5;
